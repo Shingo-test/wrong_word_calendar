67ebbccdac3f87268b5e36ed02c9f769198a37fd
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2023-04-12 18:36:33 +0900

    ワードのランダム出力のロジック書きました

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import MainImage from "./MainImage";
+import WrongWord from "./WrongWord";
 import "./App.css";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <h1>
         Toma Kei wrong word <a href="#calendar">calendar</a>
       </h1>
-      <MainImage />
+      {/* <MainImage /> */}
+      <WrongWord />
       <div className="calendar" id="calendar">
         <FullCalendar
           plugins={[dayGridPlugin]}
